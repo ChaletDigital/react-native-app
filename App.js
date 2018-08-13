20241ad03bba34ed49c3aa80322775558893ec98
Eugenio <geninhofloripa@hotmail.com>
2018-08-13 07:34:58 -0300

    Refactor: more scalable code to show multiple pins

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View,Text,StyleSheet,Switch} from 'react-native';
 import { Constants } from 'expo';
+
 const ardBoard =  'http://geninhofloripa.ddns.net:82/PIN';
+
 const styles = StyleSheet.create({
     pinContainer: {
         flexDirection: 'row',
@@ -25,69 +27,53 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            worklightPin: { id: 62, text: 'WorkLights', checked: false },
-            desklightPin: { id: 48, text: 'DeskLights', checked: false },
+            pins: {
+                workLight: { id: 62, text: 'Work Lights', checked: false },
+                deskLight: { id: 48, text: 'Desk Lights', checked: false },
+                portchLight: { id: 26, text: 'Portch Lights', checked: false },
+            }
         };
     }
 
-    setStateAsync(state) {
-        return new Promise((resolve) => {
-            this.setState(state, resolve)
-        });
+    togglePinState(key) {
+        this.setState((prevState) => {
+            //update internal state
+            pins: {
+                ...prevState.pins,
+                [key] : {
+                    id: prevState.pins[key].id,
+                    text: prevState.pins[key].text,
+                    checked: !prevState.pins[key].checked
+                }
+            }
+        }, (state) => {
+            //call Arduino wit new state
+            this.updatePinOnArduino(state.pins[key]);
+        })
     }
 
-    toggleDesklightState() {
-        this.updateDesklightState(!this.state.desklightPin.checked);
-        this.setState({
-            desklightPin: {
-                id: this.state.desklightPin.id,
-                text: this.state.desklightPin.text,
-                checked: !this.state.desklightPin.checked,
-            },
-        });
-    }
-
-    updateDesklightState(newState) {
-        const action = newState ? 'ON' : 'OFF'
-        const url = ardBoard + this.state.desklightPin.id + '=' + action;
+    updatePinOnArduino(pin) {
+        const action = pin.checked ? 'ON' : 'OFF';
+        const url = ardBoard + pin.id + '=' + action;
 
         fetch(url)
-        .then(response => console.log('DeskLight is ' + action))
-        .done();
-    }
-
-    toggleWorklightState() {
-        this.updateWorklightState(!this.state.worklightPin.checked);
-        this.setState({
-            worklightPin: {
-                id: this.state.worklightPin.id,
-                text: this.state.worklightPin.text,
-                checked: !this.state.worklightPin.checked,
-            },
-      });
-    }
-
-    updateWorklightState(newState) {
-        const action = newState ? 'ON' : 'OFF'
-        const url = ardBoard + this.state.worklightPin.id + '=' + action;
-
-        fetch(url)
-        .then(response => console.log('WorkLight is ' + action))
+        .then(response => console.log(pin.text + ' is ' + action))
         .done();
     }
 
     render() {
+        const pins = this.state.pins;
+        console.log(pins);
         return (
             <View style={styles.appContainer}>
                 <Text>Chalet Digital – Beta MVP – React-Native App</Text>
-                <Pin
-                    onToggle={() => this.toggleWorklightState()}
-                    pin={this.state.worklightPin}
-                />
-                <Pin
-                    onToggle={() => this.toggleDesklightState()}
-                    pin={this.state.desklightPin}
-                />
+                {Object.keys(pins).map((key) => (
+                    <Pin
+                      key={key}
+                      onToggle={this.togglePinState(key)}
+                      pin={pins[key]}
+                    />
+                ))}
             </View>
         );
     }
